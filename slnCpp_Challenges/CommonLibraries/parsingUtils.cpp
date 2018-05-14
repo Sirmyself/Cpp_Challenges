25f447a358b81d3048fcce50f8dc87fc08b15a9e
Sirmyself <jerome_masmor@hotmail.com>
2018-05-14 13:48:44 -0400

    Update parsingUtils.cpp
    `checkCorrespondingParenthesis()` now supports both both opening and closing corresponding parenthesis search.

--- slnCpp_Challenges/CommonLibraries/parsingUtils.cpp
+++ slnCpp_Challenges/CommonLibraries/parsingUtils.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-size_t checkCorrespondingParenthesis(const string pFormula, unsigned int firstIndex)
+//following here is the old algorithm for reference purpose.
+/*size_t checkCorrespondingParenthesis(const string pFormula, unsigned int firstIndex)
 {
 	unsigned int i = firstIndex;
 	unsigned int opening = 0, closing = 0;
@@ -17,6 +18,41 @@ size_t checkCorrespondingParenthesis(const string pFormula, unsigned int firstIn
 			return i;
 		else
 			++i;
+	}
+
+	return -5;
+}*/
+
+//here is the new algorithm to support both opening and closing parenthesis search.
+size_t checkCorrespondingParenthesis(const string pFormula, unsigned int firstIndex)
+{
+	unsigned int i = firstIndex;
+	unsigned int opening = 0, closing = 0;
+	
+	int mod = 0;
+	if (pFormula[i] == '(')
+	{
+	    mod = 1;
+	}
+	else if (pFormula[i] == ')')
+	{
+	    mod = -1;
+	}
+	else
+	{
+	    return -10; //probably need to change in order to have a proper error code.
+	                //error will be "Error : Given index was not a parenthesis"
+	}
+
+	while (i < pFormula.length())
+	{
+		if (pFormula[i] == '(') ++opening;
+		if (pFormula[i] == ')') ++closing;
+
+		if (opening == closing)
+			return i;
+		else
+			i += mod;
 	}
 
 	return -5;
